87ec4a4d0d28227e54716169cec64d638a42740c
Mohammad Arfizur Rahman <arfizrahman0@gmail.com>
2021-02-07 22:39:01 +0600

    Add order context and implement db seed method

--- src/Ordering/Ordering.Infrastructure/Data/OrderContext.cs
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContext.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
 
 namespace Ordering.Infrastructure.Data
 {
-    class OrderContext
+    public class OrderContext : DbContext
     {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
     }
 }
