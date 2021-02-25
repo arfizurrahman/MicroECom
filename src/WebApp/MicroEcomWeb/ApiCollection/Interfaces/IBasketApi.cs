09f2dd412c98babf1652c5afb831d7571dc24f38
Mohammad Arfizur Rahman <arfizrahman0@gmail.com>
2021-02-25 08:43:11 +0600

    Added api builder classes and api interfaces

---
+++ src/WebApp/MicroEcomWeb/ApiCollection/Interfaces/IBasketApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroEcomWeb.ApiCollection.Interfaces
+{
+    public interface IBasketApi
+    {
+        Task<BasketModel> GetBasket(string userName);
+        Task<BasketModel> GetBasket(BasketModel model);
+        Task CheckoutBasket(BasketCheckoutModel model);
+    }
+}
---
+++ src/WebApp/MicroEcomWeb/ApiCollection/Interfaces/IOrderApi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroEcomWeb.ApiCollection.Interfaces
+{
+    public interface IOrderAPi
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrderByUserName(string userName);
+    }
+}
